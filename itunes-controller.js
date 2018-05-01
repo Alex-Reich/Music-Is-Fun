d01641879658ecb1024c3374a5c39fbce92b4d78
Alex Reich <atrain.reich@gmail.com>
2018-05-01 16:39:40 -0600

    Functional with minimal styling

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,19 +1,44 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
+    itunesService.getMusicByArtist(artist).then(drawSongs); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
-  function draw(results){
-    console.log(results)
+  function drawSongs(results) {
+    var songs = results
+    var template = ""
+    for (var i = 0; i < songs.length; i++) {
+      var song = songs[i];
+
+      template += `
+      <div class="row">
+        <div class="col">
+          <div class="card align-items-center card-bg">
+          <img src="${song.albumArt}" alt="">
+          <div class="card-body">
+              <h3>Artist: ${song.artist}</h3>
+              <h3>Song: ${song.title}</h3>
+              <h3>Album: ${song.collection}</h3>
+              <Button>Price: ${song.price}</Button>
+              <audio controls>
+                <source src="${song.preview}" type="audio/ogg">
+                Your browser does not support the audio tag.
+              </audio>
+          </div>
+        </div>
+        </div>
+      </div>
+      `
+    }
+    document.getElementById("songs").innerHTML = template
+
+
+
+
   }
 
-
-
-
-  
 }
